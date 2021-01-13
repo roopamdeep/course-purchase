88fcffb202db806f2b51e913784a6a7dc9c10a50
Roopamdeep Kaur <roopam_kaur@yahoo.in>
2021-01-12 21:46:51 -0500

    added course component

--- src/CourseSales.js
+++ src/CourseSales.js
@@ -34,9 +34,27 @@ class CourseSales extends Component {
   }
 }
 class Course extends Component {
-  state = {};
+  clicker() {
+    var active = !this.state.active;
+    this.setState({ active: active });
+    this.props.sumPrice(active ? this.props.price : -this.props.price);
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+    };
+    this.clicker = this.clicker.bind(this);
+  }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <p className={this.state.active ? "active" : ""} onClick={this.clicker}>
+          {this.props.name}
+          <b>{this.props.price}</b>
+        </p>
+      </div>
+    );
   }
 }
 
